e374caf8c66b2c39d3c3e6e28ea6187d84610148
Andre de Beer <andreb@microsoft.com>
2021-12-04 20:00:53 +1300

    Day4 complete

---
+++ avent4-1.py
@@ -0,0 +1,58 @@
+# Part 1
+
+import numpy as np
+
+def wincard(card):
+    win = False
+    for i in range(5):
+        win = win | (sum(card[i]) == 50000) | (sum(card[:,i]) == 50000)
+    return win
+
+
+bcards = [[ [0] * 5 ] * 5] * 100
+tab = -1    
+col = 0
+row = 0
+bcalls = [0] * 100
+cards = np.array(bcards)
+calls = np.array(bcalls)
+f = open("input4", "r")
+cntr = 0
+for line in f:
+    if cntr == 0:
+        n = 0
+        for num in line.split(','):
+            calls[n] = int(num)
+            n += 1
+    else:
+        if line == "\n":
+            tab += 1
+            row = 0
+        else:
+            col = 0
+            for num in line.split(' '):
+                if (num != "\n") & (num != ""):
+                    cards[tab, row, col] = int(num)
+                    col += 1
+            row += 1    
+    cntr += 1
+    #print(cards)
+f.close()
+win = False
+call = 0
+while not(win) & (call < 100):
+    tab = 0
+    while not(win) and (tab < 100):
+        for row in range(5):
+            for col in range(5):
+                if cards[tab,row,col] == calls[call]:
+                    cards[tab,row,col] = 10000
+        win = wincard(cards[tab])
+        tab += 1
+    call +=1
+    
+lastcall = calls[call-1]
+boardtot = sum(sum(cards[tab-1])) % 10000 
+score = lastcall * boardtot
+print("Score:", score)
+
